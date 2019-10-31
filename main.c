6ab0dd982c45257f14c0172aca76468f5a9c8cad
SUP3RS0N1C <sputnik1957@sookmyung.ac.kr>
2019-10-31 15:31:36 +0900

    03-2

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) 
 {
 	int i;
-	int grade[SIZE];
-	int score[SIZE];	
+	int a[5]={1,2,3,4,5};
+	int b[5]={1,2,3,4,5};
+	int flag_same = 1;
 	
-	for(i=0;i<SIZE; i++)
-		grade[i] = rand()%100;
-	
-	for (i=0; i<SIZE; i++)
-		score[i] = grade[i];
-	
-	for (i=0; i<SIZE; i++)
-		printf("score[%d] = %d\n", i, score[i]);
-
+	for(i=0;i<5; i++)
+	{
+		if(a[i]!=b[i])
+			printf("%d is different\n", i);
+	}
 	return 0;
 }
